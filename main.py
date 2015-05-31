4a308aaf74d1d34a336f5a802e2f56f014680423
swacky <student@Someone.local>
2015-05-31 12:15:54 +1000

    make  it look nicer but do the same thing

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from browser import document,window,timer,alert,html
 texts = ["LOL", "Swacky"]
-repeat = 0
-def flashText():
-    global repeat
-    document['title'].set_text(texts[repeat])
-    repeat += 1
-    if repeat >= len(texts):
-        repeat = 0
+class FlashText:
+    def __init__(self,time):
+        self.repeat = 0
+        self.time = time
+        self.interval = timer.set_interval(self.flash,1000)
         
-print('got here',dir(document['title']) ) #,document['title'].value)
-timer.set_interval(flashText,1000)
+    def flash(self):
+        document['title'].set_text(texts[self.repeat])
+        self.repeat += 1
+        if self.repeat >= len(texts):
+            self.repeat = 0
+        
+flashText = FlashText(1000)        
+#print('got here',dir(document['title']) ) #,document['title'].value)
+
 #document['title'].set_text("hello Everyboody this written in python, I hope it will work, LOL")
